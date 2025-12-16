4ac0dd495f0dab4e56769b37b63a08c3980820d2
Analyser <analyser@gmail.com>
2025-12-16 17:18:18 +0800

    chore(release): bump version to 0.2.4-beta1

--- cloud_cert_renewer/__init__.py
+++ cloud_cert_renewer/__init__.py
@@ -1,5 +1,5 @@
 """Cloud Certificate Renewer - Automated HTTPS certificate renewal tool for cloud services."""  # noqa: E501
 
-__version__ = "0.2.3-beta1"
+__version__ = "0.2.4-beta1"
 __author__ = "analyser"
 __email__ = "analyser@gmail.com"
